ebbd567836629fb7f54c9309da03ebf59348c140
Luis Rodriguez <developer0606@gmail.com>
2019-10-31 21:40:55 -0400

    Controllers Added, Views

---
+++ ITLA_Tarea1/ITLA_Tarea1/Controllers/EquiposController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ITLA_Tarea1.Models;
+
+namespace ITLA_Tarea1.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private ITLA_Tarea1Context db = new ITLA_Tarea1Context();
+
+        // GET: Equipos
+        public ActionResult Index()
+        {
+            var equipos = db.Equipos.Include(e => e.Division);
+            return View(equipos.ToList());
+        }
+
+        // GET: Equipos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipos.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // GET: Equipos/Create
+        public ActionResult Create()
+        {
+            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name");
+            return View();
+        }
+
+        // POST: Equipos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EquipoId,Name,DivisionId,Localization,Year")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Equipos.Add(equipo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name", equipo.DivisionId);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipos.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name", equipo.DivisionId);
+            return View(equipo);
+        }
+
+        // POST: Equipos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EquipoId,Name,DivisionId,Localization,Year")] Equipo equipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name", equipo.DivisionId);
+            return View(equipo);
+        }
+
+        // GET: Equipos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Equipo equipo = db.Equipos.Find(id);
+            if (equipo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipo);
+        }
+
+        // POST: Equipos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Equipo equipo = db.Equipos.Find(id);
+            db.Equipos.Remove(equipo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
